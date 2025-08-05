8fe1da7fffd6037b23de66c32c582a7f5ae6c29e
Gerson Kurz <gerson.kurz@ng-branch-technology.com>
2025-08-05 16:21:15 +0200

    Make virtual folders start from, well, virtual folders

--- Database/Nodes/VirtualFoldersOverview.cpp
+++ Database/Nodes/VirtualFoldersOverview.cpp
@@ -15,24 +15,47 @@ namespace jucyaudio
         VirtualFoldersOverview::VirtualFoldersOverview(INavigationNode *parent)
             : BaseNode{parent, "Folders", "Folder", "Folders"}
         {
-            // The name "Folders" will appear in the navigation tree
         }
 
         bool VirtualFoldersOverview::expand(std::vector<INavigationNode *> &outChildren)
         {
-            const auto &folderDb = theTrackLibrary.getFolderDatabase();
+            // Get the required database managers from the central library instance.
+            auto &rootManager = theTrackLibrary.getLibraryRootManager();
+            auto &folderDb = theTrackLibrary.getFolderDatabase();
 
-            // Get the root folders (parentId = -1) from our new, fast, cached folder database.
-            const auto rootFolders = folderDb.getChildFolders(-1);
+            // 1. Get the list of user-defined root paths from our new manager.
+            const auto libraryRoots = rootManager.getAllRoots();
 
-            for (const auto &folderInfo : rootFolders)
+            // 2. For each configured root path, create a display node.
+            for (const auto &rootInfo : libraryRoots)
             {
-                // Create the new VirtualFolderNode for each root folder.
-                outChildren.push_back(new VirtualFolderNode{this, folderInfo});
+                // To create a VirtualFolderNode, we need the full FolderInfo struct.
+                // We can get this by looking up the folder by its path.
+                // NOTE: This call is robust; if a root was added but never scanned,
+                // this will ensure its entry exists in the Folders table.
+                const FolderId folderId = folderDb.findOrCreateFolderByPath(rootInfo.path);
+
+                if (folderId != -1)
+                {
+                    // Now that we have the ID, get the full info object.
+                    // This is fast, as it should hit the folder cache.
+                    if (auto folderInfoOpt = folderDb.getFolderById(folderId))
+                    {
+                        outChildren.push_back(new VirtualFolderNode{this, *folderInfoOpt});
+                    }
+                    else
+                    {
+                        spdlog::warn("Could not retrieve FolderInfo for a known root path '{}' with ID {}", rootInfo.path, folderId);
+                    }
+                }
+                else
+                {
+                    spdlog::warn("Could not find or create a folder entry for root path '{}'", rootInfo.path);
+                }
             }
 
-            spdlog::debug("VirtualFoldersOverview loaded {} root folders from the database cache.", rootFolders.size());
-            return !rootFolders.empty();
+            spdlog::debug("VirtualFoldersOverview loaded {} custom root folders.", libraryRoots.size());
+            return !libraryRoots.empty();
         }
 
         const DataActions &VirtualFoldersOverview::getNodeActions() const
