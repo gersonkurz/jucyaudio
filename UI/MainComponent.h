9701a1faa5233095b448b6bbc77e8b68a50bc176
Gerson Kurz <gerson.kurz@pm.me>
2025-08-17 15:38:45 +0200

    Followup: remove duplicate from working set
    Signed-off-by: Gerson Kurz <gerson.kurz@pm.me>

--- UI/MainComponent.h
+++ UI/MainComponent.h
@@ -83,6 +83,7 @@ namespace jucyaudio
 
             // --- export mix functionality ---
             void onExportMix(INavigationNode *selectedNode);
+            void onRemoveDuplicates(INavigationNode* selectedNode);
             void onExportMixSettingsReceived(const MixInfo &mixInfo, const audio::ActiveExportSettings &settings);
             std::unique_ptr<juce::FileChooser> m_activeFileChooser;
 
